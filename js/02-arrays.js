6cb53c3a8a53cad442a1207909a241f88039e1a2
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-04-16 17:14:42 -0300

    exercicio array

---
+++ js/02-arrays.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const alunos = ["Wilian", "Vinicius", "Marli", "Duda", "Gabriel"];
+
+// Exibindo a estrutura do array (somente no console)
+console.log(alunos);
+
+// Acessando individualmente alguns alunos
+console.log(alunos[1]); // Vinicius
+console.log(alunos[3]); // Duda
+console.log(alunos[0]); // Wilian
+
+const favoritos = ["violão", "guitarra", "rock", "sorvete", "Dark souls", "Metallica", "Megadeth"];
+
+console.log(favoritos);
+
+console.log(`estava tocando ${favoritos[1]}, após isso joguei ${favoritos[4]} escutando ${favoritos[6]}`);
