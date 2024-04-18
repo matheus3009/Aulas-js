aef07a343e26f14cda341334ff58f07b22eaad75
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-04-18 15:29:21 -0300

    novo operador logico

--- js/05-operadores-logicos.js
+++ js/05-operadores-logicos.js
@@ -18,3 +18,23 @@ if (mediaFinal >= mediaMinima && faltas <= limiteDeFaltas) {
 }
 
 console.log(resultado)
+
+console.log("--------------------------------");
+
+let idade = 25;
+
+// Operador OU/OR (||) obs: simbolo | PIPE
+if (idade >= 18 || idade < 12) {
+    console.log("Você é um adulto ou criança!");
+}else {
+    console.log("Você é um adolescente!")
+}
+
+let onibus =  2203;//; 2203 OU 2019
+
+if (onibus == 2203 || onibus == 2019) {
+    console.log("Passa no Senac, embarque!");
+    
+} else {
+    console.log("Não passa no Senac, aguarde!")    
+}
