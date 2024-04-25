4ef1879b3efe512af585fb5dea555eb185272ef7
matheus3009 <matheussilvaborgessousa@gmail.com>
2024-04-25 15:07:28 -0300

    Create 09-eventos.js

---
+++ js/09-eventos.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//seleção de elemento
+const exemplo01 = document.querySelector("#exemplo01");
+
+//criando um Ouvinte de Evento (Event Listener)
+/* O primeiro parâmetro do addEventListener é o tipo de evento que queremos "ouvir/monitorar". O segundo por executar ações no momento em que o evento acontecer. */
+
+exemplo01.addEventListener("click", function(){
+    alert("")
+});
